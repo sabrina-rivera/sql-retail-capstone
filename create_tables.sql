b58d0419191787103f47fa9d70e7491813cf1b1b
Sabrina R <sabrinarivera31@gmail.com>
2025-12-31 16:54:12 -0500

    Tables

---
+++ create_tables.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE superstore_raw (
+    order_id TEXT,
+    order_date DATE,
+    ship_date DATE,
+    customer_id TEXT,
+    customer_name TEXT,
+    segment TEXT,
+    region TEXT,
+    state TEXT,
+    city TEXT,
+    category TEXT,
+    sub_category TEXT,
+    product_name TEXT,
+    sales REAL,
+    quantity INTEGER,
+    profit REAL
+
